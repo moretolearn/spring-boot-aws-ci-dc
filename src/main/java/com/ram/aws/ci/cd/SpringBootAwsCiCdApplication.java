69d2051c6064d8812b380de5b63fa263bb15efa0
moretolearn <rammohanreddy.yanumula@virsham.com>
2020-11-26 15:05:07 +0530

    intial commit

---
+++ src/main/java/com/ram/aws/ci/cd/SpringBootAwsCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.ram.aws.ci.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ram/aws/ci/cd/SpringBootAwsCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.aws.ci.cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
